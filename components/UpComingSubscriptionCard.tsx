3cde603aae758222c535b8a0ec530c070e2e8adb
Surendra Budhathoki <surendra@pahadi.net>
2026-04-01 21:09:58 +0545

    Fix UpcomingSubscription card design

--- components/UpComingSubscriptionCard.tsx
+++ components/UpComingSubscriptionCard.tsx
@@ -8,12 +8,12 @@ const UpComingSubscriptionCard = ({name, price, currency, daysLeft, icon} : Upco
             <View className="upcoming-row">
                 <Image source={icon} className="upcoming-icon" />
                 <View>
+                    <Text className="upcoming-price">{formatCurrency(price, currency)}</Text>
                     <Text className="upcoming-meta" numberOfLines={1}>
                         {daysLeft > 1 ? `${daysLeft} days left` : `Last day`}
                     </Text>
                 </View>
             </View>
-            <Text className="upcoming-price">{formatCurrency(price, currency)}</Text>
             <Text className="upcoming-name" numberOfLines={1}>{name}</Text>
         </View>
     )
