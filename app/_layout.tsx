9585872bf5c977b5a1d912fde2fa581614c2339c
Surendra Budhathoki <surendra@pahadi.net>
2026-04-01 16:08:01 +0545

    Implement fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import {SplashScreen, Stack} from "expo-router";
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded)  return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
